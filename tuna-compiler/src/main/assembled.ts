bb96b5bf36127fea718e93083f082ce8a81cef8c
Jeremy Drouillard <jadrouil@umich.edu>
2020-12-10 09:11:30 -0800

    Compiler fully supports function
    Release compiler

--- tuna-compiler/src/main/assembled.ts
+++ tuna-compiler/src/main/assembled.ts
@@ -1,11 +1,22 @@
-import {Transformer, Transform, Manifest, OPSIFY_MANIFEST, MONGO_GLOBAL_ABSTRACTION_REMOVAL, MONGO_UNPROVIDED_LOCK_CALCULATOR} from 'conder_core'
+import {
+    Transformer, 
+    Transform, 
+    Manifest, 
+    OPSIFY_MANIFEST, 
+    MONGO_GLOBAL_ABSTRACTION_REMOVAL, 
+    MONGO_UNPROVIDED_LOCK_CALCULATOR, 
+    StrongServerEnv,
+    Var,
+    ServerEnv,
+    schemaFactory
+} from 'conder_core'
+// Need to export this
 import { LockRequirements } from 'conder_core/dist/src/main/abstract/mongo_logic/lock_calculation'
-// need to export this from core
-import { schemaFactory, ServerEnv, StrongServerEnv, Var } from 'conder_core/dist/src/main/ops'
-import {Parser} from './parser'
-import {semantify } from './semantics'
 
-export const TUNA_TO_MANIFEST = new Transformer<string, Manifest>(str => {
+import {Parser} from './parser'
+import {semantify, PrivateFuncs } from './semantics'
+
+export const TUNA_TO_MANIFEST = new Transformer<string, Manifest & PrivateFuncs>(str => {
     const p = new Parser(str).parse()
     return semantify(p, false)
 })
@@ -25,14 +36,23 @@ export const STRINGIFY_ENV: Transform<StrongServerEnv, Omit<ServerEnv, Var.MONGO
 })
 
 export const TUNA_TO_ENV: Transform<string, StrongServerEnv> = TUNA_TO_MANIFEST
-.then(OPSIFY_MANIFEST)
+.then(new Transformer(man => {
+    const manifest = OPSIFY_MANIFEST.run(man)
+    return {
+        ...manifest,
+        privateFuncs: man.privateFuncs
+    }
+}))
 .then(new Transformer(man => {
     const STORES: StrongServerEnv["STORES"] = {}
     man.globals.forEach((v, k) => STORES[k] = schemaFactory.Any)
 
     const PROCEDURES: StrongServerEnv["PROCEDURES"] = {}
+    const PRIVATE_PROCEDURES: StrongServerEnv["PRIVATE_PROCEDURES"] = [...man.privateFuncs.values()]
     man.funcs.forEach((v, k) => {
+        //@ts-ignore
         PROCEDURES[k] = v
+        
     })
 
     const SCHEMAS: StrongServerEnv["SCHEMAS"] = []
@@ -41,7 +61,8 @@ export const TUNA_TO_ENV: Transform<string, StrongServerEnv> = TUNA_TO_MANIFEST
         DEPLOYMENT_NAME: "local-run",
         STORES,
         PROCEDURES,
-        SCHEMAS
+        SCHEMAS,
+        PRIVATE_PROCEDURES
     }
 }))
 
